c6a2db0267e0627b2d47def90aedae05358a20b7
Jane <jane.ronk@gmail.com>
2025-09-17 15:01:09 +0300

    progress

--- game.js
+++ game.js
@@ -3,27 +3,27 @@ let currentPlayerCardStats = null;
 let currentComputerCardStats = null;
 
 const allCards = {
-   monsters:  [
-    { name: "stygimoloch", hp: 5, attack: 3, points: 3 },
-    { name: "triceratops", hp: 8, attack: 4, points: 5 },
-    { name: "velociraptor", hp: 3, attack:2, points: 2 },
-    { name: "tyrannosaurus rex", hp: 10, attack: 5, points: 8 },
-    { name: "pteranodon", hp: 4, attack: 2, points: 3 },
-    { name: "spinosaurus", hp: 9, attack: 4, points: 7 },
-    { name: "ankylosaurus", hp: 10, attack: 3, points: 6 },
-    { name: "dilophosaurus", hp: 6, attack: 3, points: 4 },
-    { name: "baryonyx", hp: 8, attack: 4, points: 5 },
-    { name: "carnotaurus", hp: 7, attack: 4, points: 5 },
-    { name: "allosaurus", hp: 8, attack: 4, points: 6 },
-    { name: "giganotosaurus", hp: 12, attack: 5, points: 9 },
-    { name: "protocerato", hp: 5, attack: 3, points: 4 },
-    { name: "dreadnoughtus", hp: 15, attack: 7, points: 11 },
-    { name: "tupandactylus", hp: 4, attack: 2, points: 3 },
-    { name: "monolophosaurus", hp: 6, attack: 3, points: 4 },
-   ],
-   supportitems: [
-    { name: "Health potion", effect: "heal", value: 2 },
-   ]
+    monsters: [
+        { name: "stygimoloch", hp: 5, attack: 3, points: 3 },
+        { name: "triceratops", hp: 8, attack: 4, points: 5 },
+        { name: "velociraptor", hp: 3, attack: 2, points: 2 },
+        { name: "tyrannosaurus rex", hp: 10, attack: 5, points: 8 },
+        { name: "pteranodon", hp: 4, attack: 2, points: 3 },
+        { name: "spinosaurus", hp: 9, attack: 4, points: 7 },
+        { name: "ankylosaurus", hp: 10, attack: 3, points: 6 },
+        { name: "dilophosaurus", hp: 6, attack: 3, points: 4 },
+        { name: "baryonyx", hp: 8, attack: 4, points: 5 },
+        { name: "carnotaurus", hp: 7, attack: 4, points: 5 },
+        { name: "allosaurus", hp: 8, attack: 4, points: 6 },
+        { name: "giganotosaurus", hp: 12, attack: 5, points: 9 },
+        { name: "protocerato", hp: 5, attack: 3, points: 4 },
+        { name: "dreadnoughtus", hp: 15, attack: 7, points: 11 },
+        { name: "tupandactylus", hp: 4, attack: 2, points: 3 },
+        { name: "monolophosaurus", hp: 6, attack: 3, points: 4 },
+    ],
+    supportitems: [
+        { name: "Health potion", effect: "heal", value: 2 },
+    ]
 };
 // ------------------------------
 monscount = 0;
@@ -35,12 +35,12 @@ const computerHandDiv = document.getElementById("computer-hand");
 const drawButton = document.getElementById("draw-button");
 const playerPointsElem = document.getElementById("player-points");
 const computerPointsElem = document.getElementById("computer-points");
-testbutton.disabled = true;
+const start = document.getElementById("start");
 let playerPoints = 0;
 let computerPoints = 0;
 let i = 0;
 chosen = null;
-
+count = 0;
 
 
 
@@ -75,8 +75,10 @@ function handlePlayerCardClick() {
     for (let o = 0; o < cardDivs.length; o++) {
         cardDivs[o].style.border = "";
         cardDivs[o].id = "";
+        cardDivs[o].style.scale = "1";
     }
-    this.style.border = "2px solid blue";
+    this.style.border = "2px solid #47EB50";
+    this.style.scale = "1.2"
     this.setAttribute("id", "selected-card");
 }
 
@@ -104,19 +106,19 @@ function lockIn() {
     const playerHp = parseInt(hp.replace(/\D/g, ''), 10);
     const playerAtk = parseInt(atk.replace(/\D/g, ''), 10);
     const playerPoints = parseInt(points.replace(/\D/g, ''), 10);
-    
-    document.querySelector("#selected-card").style.scale = "1.2";
+    count++;
 
     console.log(playerHp);
     console.log(playerAtk);
     console.log(playerPoints);
 
     removePlayerCardListeners();
-    testbutton.disabled = false;
+
     currentPlayerCardStats = { playerHp, playerAtk, playerPoints };
-    
+
     console.log("started attack");
     lockInButton.disabled = true;
+    testbutton.disabled = false;
 
 }
 resultEnemyHp = null;
@@ -125,42 +127,38 @@ resultPlayerHp = null;
 testbutton.addEventListener("click", startAttack);
 
 function startAttack() {
-    if (currentPlayerCardStats == null){
+    if (currentPlayerCardStats == null) {
         return console.log("no active selected");
     }
     else 
-
-        // 
-    document.getElementById("selected-card").style.scale= "1.2";
-        //
+    changeStyle();    
+    // document.getElementById("selected-card").style.scale = "1.2";
     resultEnemyHp = currentComputerCardStats.hpNum - currentPlayerCardStats.playerAtk;
     resultPlayerHp = currentPlayerCardStats.playerHp - currentComputerCardStats.atkNum;
     console.log("remaining enemy hp " + resultEnemyHp);
     currentComputerCardStats.hpNum = resultEnemyHp;
     currentPlayerCardStats.playerHp = resultPlayerHp;
-    testbutton.disabled= true;
-
+    testbutton.disabled = true;
+    setTimeout(() => {
         // document.getElementById(rand).style.scale = "1.4";
-
-        
-        document.querySelector("#" + rand +  " .hp").innerHTML = "HP: " + resultEnemyHp;
+        document.querySelector("#" + rand + " .hp").innerHTML = "HP: " + resultEnemyHp;
         document.querySelector("#selected-card .hp").innerHTML = "HP: " + resultPlayerHp;
         setTimeout(() => {
-           
+
             if (resultEnemyHp <= 0) {
-            playerPoints = playerPoints + currentComputerCardStats.pointsNum;
-            playerPointsElem.innerHTML = playerPoints;
-            console.log("added " + currentComputerCardStats.pointsNum + " to playerpoints")
-                     testbutton.disabled= true;
-                     document.getElementById(rand).remove();
-                     const indexToRemove = enemyMonsters.indexOf(rand);
-                    monscount = monscount - 1;
-                    console.log(monscount + " count")
+                playerPoints = playerPoints + currentComputerCardStats.pointsNum;
+                playerPointsElem.innerHTML = playerPoints;
+                console.log("added " + currentComputerCardStats.pointsNum + " to playerpoints")
+                testbutton.disabled = true;
+                document.getElementById(rand).remove();
+                const indexToRemove = enemyMonsters.indexOf(rand);
+                monscount = monscount - 1;
+                console.log(monscount + " count")
+                testbutton.disabled = true;
                 setTimeout(() => {
                     if (monscount == 0) {
-                    enemyMonsters = ["computer-card-0", "computer-card-1", "computer-card-2", "computer-card-3"];
-                    rand = enemyMonsters[~~(Math.random() * enemyMonsters.length)];
-                // document.getElementById(rand).style.scale = "1.2";
+                        enemyMonsters = ["computer-card-0", "computer-card-1", "computer-card-2", "computer-card-3"];
+                        rand = enemyMonsters[~~(Math.random() * enemyMonsters.length)];
 
                         computerCardsGenerated();
                     }
@@ -168,35 +166,38 @@ function startAttack() {
 
                 if (indexToRemove > -1) {
                     enemyMonsters.splice(indexToRemove, 1);
-                }   
-                    setTimeout(() => {
-                        rand = enemyMonsters[~~(Math.random() * enemyMonsters.length)];
-                        compActiveCard(); 
-                    }, 1000);
+                }
+                setTimeout(() => {
+                    rand = enemyMonsters[~~(Math.random() * enemyMonsters.length)];
+                    compActiveCard();
+                }, 1000);
 
-        }   
-        if (resultPlayerHp <=0) {
-            computerPoints = computerPoints + currentPlayerCardStats.playerPoints;
-            computerPointsElem.innerHTML = computerPoints;
-                     testbutton.disabled= true;
-                     lockInButton.disabled= false; 
-            document.getElementById("selected-card").remove();
-            x = x + 1;
-            if (x == 4 && deck.length < 4){
-                gameEnd();
             }
-            document.getElementById(rand).style.scale = "1.2";               
+            if (resultPlayerHp <= 0) {
+                computerPoints = computerPoints + currentPlayerCardStats.playerPoints;
+                computerPointsElem.innerHTML = computerPoints;
+                testbutton.disabled = true;
+                lockInButton.disabled = false;
+                document.getElementById("selected-card").remove();
+                x = x + 1;
+                if (x == 4 && deck.length < 4) {
+                    gameEnd();
+                }
 
-            checkX();
-            activeCard(playerHandDiv)
-        }
-        else {
-                    testbutton.disabled= false;
-                    // document.getElementById(rand).style.scale = "1.2";
-}                    
-             console.log("next round");
+
+                checkX();
+                activeCard(playerHandDiv)
+            }
+            else {
+                console.log("peekaboo");
+                setTimeout(() => {
+                    testbutton.disabled = false;
+                }, 300);
+            }
+            console.log("next round");
         }, 1000);
-    
+    }, 1000);
+
 }
 
 
@@ -207,11 +208,12 @@ function checkX() {
     else return;
 }
 function drawPlayerCards() {
+    lockInButton.disabled = false;
     if (deck.length < 4) return;
     const playerMonsters = drawMonsters(deck, 4);
     renderHand(playerHandDiv, playerMonsters);
     activeCard(playerHandDiv);
-           x = 0; 
+    x = 0;
     checkX();
 
 
@@ -219,12 +221,12 @@ function drawPlayerCards() {
 }
 
 function computerCardsGenerated() {
-    setTimeout(() => { 
+
         if (deck.length < 4) {
 
             gameEnd();
         }
-        shuffleDeck(deck);        
+        shuffleDeck(deck);
         const computerMonsters = drawMonsters(deck, 4);
         renderHand(computerHandDiv, computerMonsters);
 
@@ -236,35 +238,43 @@ function computerCardsGenerated() {
         }
         console.log(rand);
 
+
+            compActiveCard();
         setTimeout(() => {
-        compActiveCard();
-}, 1000)
-    }, 500)
+        document.getElementById(rand).style.scale = "1.2";
+            document.getElementById(rand).style.borderColor = "#47eb50";
+        document.getElementById(rand).style.zIndex = "10";
+
+            
+        }, 500);
+
 }
 
-function compActiveCard(){
-        chosen = document.getElementById(rand);
+function compActiveCard() {
+    chosen = document.getElementById(rand);
 
-        const hp = chosen.querySelector(".hp").innerHTML;
-        const atk = chosen.querySelector(".atk").innerHTML;
-        const points = chosen.querySelector(".points").innerHTML;
-        chosen.style.scale= "1.2";
-        const hpNum = parseInt(hp.replace(/\D/g, ''), 10);
-        const atkNum = parseInt(atk.replace(/\D/g, ''), 10);
+    const hp = chosen.querySelector(".hp").innerHTML;
+    const atk = chosen.querySelector(".atk").innerHTML;
+    const points = chosen.querySelector(".points").innerHTML;
+    // chosen.style.scale = "1.2";
+    const hpNum = parseInt(hp.replace(/\D/g, ''), 10);
+    const atkNum = parseInt(atk.replace(/\D/g, ''), 10);
     const pointsNum = parseInt(points.replace(/\D/g, ''), 10);
 
 
-        console.log(hpNum);
-        console.log(atkNum);
-        console.log(pointsNum);
+    console.log(hpNum);
+    console.log(atkNum);
+    console.log(pointsNum);
     currentComputerCardStats = { hpNum, atkNum, pointsNum };
-        
+    
+    if (count >= 1)
+    testbutton.disabled = false;
 
 }
 
 function gameRound() {
     if (deck.length < 4) {
-    console.log("out of cards")
+        console.log("out of cards")
         return;
     } // Not enough cards left
     shuffleDeck(deck);
@@ -278,7 +288,7 @@ function gameRound() {
 
 drawButton.addEventListener("click", gameRound);
 
-document.addEventListener("DOMContentLoaded", computerCardsGenerated);
+
 
 // -------------------------------
 
@@ -292,24 +302,24 @@ function hide() {
 
 
 function gameEnd() {
-            if (playerPoints > computerPoints){
-                victory();
-                hide();
-            }
-            else if (computerPoints > playerPoints){
-                defeat();
-                hide();
-            }
-            else if (computerPoints == playerPoints){
-                draw();
-                hide();
-            }
+    if (playerPoints > computerPoints) {
+        victory();
+        hide();
+    }
+    else if (computerPoints > playerPoints) {
+        defeat();
+        hide();
+    }
+    else if (computerPoints == playerPoints) {
+        draw();
+        hide();
+    }
 
 }
 
 
-    const createDiv = document.createElement("div");
-    
+const createDiv = document.createElement("div");
+
 
 function victory() {
     createDiv
@@ -321,13 +331,46 @@ function victory() {
 function defeat() {
     createDiv
     createDiv.setAttribute("id", "defeat");
-        createDiv.innerHTML = "<p>defeat!!</p>"
+    createDiv.innerHTML = "<p>defeat!!</p>"
     document.body.appendChild(createDiv);
 }
 
 function draw() {
     createDiv
     createDiv.setAttribute("id", "draw");
-        createDiv.innerHTML = "<p>draw!!</p>"
+    createDiv.innerHTML = "<p>draw!!</p>"
     document.body.appendChild(createDiv);
 }
+
+
+function changeStyle() {
+    document.getElementById("selected-card").style.scale = "1.4";
+    document.getElementById(rand).style.scale = "1.4";
+    setTimeout(() => {
+        document.getElementById(rand).style.scale = "1.2";
+        document.getElementById("selected-card").style.scale = "1.2";
+    }, 500);
+}
+
+
+start.addEventListener("click", fancy)
+
+
+// ---------------------------------------
+
+function fancy(){
+    document.getElementById("start-game").style.display = "none";
+    document.querySelector("footer").style.display = "none";
+    document.querySelector("header").style.display = "none";
+
+    setTimeout(() => {
+    gameContainer.style.display = "block";    
+    document.querySelector("footer").style.display = "block";
+        document.querySelector("footer").style.backgroundColor = "#CEB5A7";
+        document.querySelector("header").style.display = "block";
+
+ 
+       computerCardsGenerated();
+    }, 2000);
+}
+
